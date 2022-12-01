7efda257dcbe77523d5fd6337e62f8d60c2a744b
Lucas de Paula Soares <l201867@dac.unicamp.br>
2022-12-01 13:33:04 -0300

    Communication between nodes with a message broker
    Broker class to send and receive messages to/from other nodes. Uses
    RabbitMQ broker

---
+++ comunicacao/node1.py
@@ -0,0 +1,38 @@
+import time
+import threading
+import pika
+
+LOCAL_NODE_NAME = 'node1'
+
+class Broker:
+  def __init__(self):
+    self.connection = pika.BlockingConnection(pika.URLParameters('amqp://guest:guest@localhost:5672/%2F')) # Blocking
+    self.channel = self.connection.channel()
+
+  def start_receiving(self):
+    self.channel.queue_declare(queue=LOCAL_NODE_NAME, auto_delete=True, arguments={'x-message-ttl' : 2000})
+    self.channel.basic_consume(queue=LOCAL_NODE_NAME, on_message_callback=callback, auto_ack=True)
+    self.channel.start_consuming() # Blocking 
+
+  def send(self, node_name, msg):
+    self.channel.queue_declare(queue=node_name, auto_delete=True, arguments={'x-message-ttl' : 2000})
+    self.channel.basic_publish(exchange='', routing_key=node_name, body=msg)
+
+def callback(ch, method, properties, body):
+        print(body)
+
+
+def thread_receive():
+    receive_broker = Broker()
+    receive_broker.start_receiving()
+
+if __name__ == "__main__":
+    receiving = threading.Thread(target=thread_receive)
+    receiving.start()
+    broker = Broker()
+    
+    while True:
+        time.sleep(4)
+        broker.send('node2', 'Hello from node 1')
+        broker.send('node3', 'Hello from node 1')
+        print("[x] Sent msg to node 2 and 3")
